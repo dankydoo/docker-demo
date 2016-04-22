df0e5f30dd4a5743cbacbfa65cf44c935b142f89
dankydoo <kevinraybooth@gmail.com>
2016-04-21 21:07:27 -0400

    fixing merge between repos

--- ContainerDemo/Controllers/HomeController.cs
+++ ContainerDemo/Controllers/HomeController.cs
@@ -10,6 +10,25 @@ namespace ContainerDemo.Controllers
     {
         public IActionResult Index()
         {
+            using (var con = new System.Data.SqlClient.SqlConnection("Data Source=kevins-kloud.eastus.cloudapp.azure.com;Initial Catalog=Northwind;User ID=sa;Password=thepassword2#"))
+            {
+                //var adapter = new SqlCommand("SELECT * FROM Categories", con);
+
+                var command = con.CreateCommand();
+                command.CommandText = "SELECT * FROM Categories";
+
+                con.Open();
+                var reader = command.ExecuteReader();
+
+                int i = 0;
+                while (reader.Read())
+                {
+                    ViewData["Data"] = ViewData["Data"] + " | " + reader.GetString(2);
+                    i++;
+                }
+
+            }
+
             return View();
         }
 
